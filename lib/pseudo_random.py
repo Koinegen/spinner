b7f10ecc6c009606c7f34f385517afb141e3fb4b
Koinegen <1337kng1337@gmail.com>
2023-02-15 17:51:45 +0300

    Commit code

---
+++ lib/pseudo_random.py
@@ -0,0 +1,101 @@
+from dataclasses import dataclass
+import random
+from typing import List
+from typing_extensions import TypedDict
+
+
+
+@dataclass
+class Product:
+    name: str
+    quantity: int
+    rarity: bool = False
+
+    def __post_init__(self):
+        if self.quantity == 1:
+            self.rarity = True
+
+
+@dataclass
+class Settings:
+    calls_limit: int
+    rare_per_limit: int
+
+
+class Response(TypedDict):
+    position: str
+
+
+class Stock:
+
+    def __init__(self):
+        self.__products: list = []
+        self.__calls = 1
+        self.__calls_limit = 15
+        self.__rare_count = 0
+        self.__rare_per_limit = 3
+
+    @property
+    def products(self):
+        return self.__products
+
+    @products.setter
+    def products(self, value: List[Product]):
+        self.__products.extend(value)
+
+
+    @property
+    def calls_limit(self):
+        return self.__calls_limit
+
+    @calls_limit.setter
+    def calls_limit(self, value: int):
+        if value >= 1:
+            self.__calls_limit = value
+        else:
+            raise Exception
+
+    @property
+    def rare_per_limit(self):
+        return self.__rare_per_limit
+
+    @rare_per_limit.setter
+    def rare_per_limit(self, value):
+        if value >= 1:
+            self.__rare_per_limit = value
+        else:
+            raise Exception
+
+    def get_product(self):
+        if len(self.__products) > 0:
+
+            while True:
+                product = random.choice(self.products)
+                if product.quantity != 0:
+                    if product.quantity == -1:
+                        self.__calls += 1
+                        return product
+                    else:
+                        if product.rarity:
+                            if self.__rare_count < (self.__rare_per_limit * (self.__calls / self.__calls_limit)):
+                                print(self.__rare_count)
+                                product.quantity -= 1
+                                self.__calls += 1
+                                self.__rare_count += 1
+                                return product
+                            else:
+                                continue
+                        else:
+                            product.quantity -= 1
+                            self.__calls += 1
+                            return product
+                else:
+                    self.products.remove(product)
+                    continue
+        else:
+            raise Exception
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from lib import pseudo_random
+from typing import List, Union
+
+
+app = FastAPI()
+randomizer = pseudo_random.Stock()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.post("/items")
+async def init_products(items: List[pseudo_random.Product]):
+    randomizer.products = items
+
+@app.get("/respawn")
+async def back_to_defaults():
+    global randomizer
+    randomizer = pseudo_random.Stock()
+
+@app.post("/settings")
+async def set_settings(settings: pseudo_random.Settings):
+    randomizer.calls_limit = settings.calls_limit
+    randomizer.rare_per_limit = settings.rare_per_limit
+
+
+@app.get("/spin")
+async def spin() -> Union[pseudo_random.Response, dict]:
+    try:
+        product = randomizer.get_product()
+    except Exception:
+        return {}
+    return {"position": product.name}
